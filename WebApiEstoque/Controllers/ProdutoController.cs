0074e670428cfd6a347d0b47d2d4c163c57fd41f
Thiago Mendes <thiago.mendes777@hotmail.com>
2023-03-12 19:38:22 -0300

    inclui try catch nos controllers para tratar possiveis erros

--- WebApiEstoque/Controllers/ProdutoController.cs
+++ WebApiEstoque/Controllers/ProdutoController.cs
@@ -21,38 +21,82 @@ namespace WebApiEstoque.Controllers
         [HttpGet]
         public async Task<ActionResult<List<ProdutoModel>>> BuscarTodosProdutos()
         {
-          List<ProdutoModel> produtos =  await  _produtosRepositorio.BuscarTodosProdutos();
+            try
+            {
+                List<ProdutoModel> produtos = await _produtosRepositorio.BuscarTodosProdutos();
             return Ok(produtos);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
 
         [HttpGet("{id}")]
         public async Task<ActionResult<ProdutoModel>> BuscarPorId(int id)
         {
+            try
+            {
             ProdutoModel produto = await _produtosRepositorio.BuscarPorId(id);
             return Ok(produto);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            
         }
 
         [HttpPost, Authorize(Roles = "administrator")]
         public async Task<ActionResult<ProdutoModel>> Cadastrar([FromBody]ProdutoModel produtoModel)
         {
+            try
+            {
            ProdutoModel produto = await _produtosRepositorio.Adicionar(produtoModel);
             return Ok(produto);
+        }
+            catch(System.Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+            
+           
         }
 
         [HttpPut("{id}"), Authorize(Roles = "administrator")]
         public async Task<ActionResult<ProdutoModel>> Atualizar([FromBody] ProdutoModel produtoModel, int id)
         {
+            try
+            {
             produtoModel.Id = id;
-            ProdutoModel produto = await _produtosRepositorio.Atualizar(produtoModel,id);
+                ProdutoModel produto = await _produtosRepositorio.Atualizar(produtoModel, id);
             return Ok(produto);
+        }
+
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpDelete("{id}"), Authorize(Roles = "administrator")]
         public async Task<ActionResult<ProdutoModel>> Apagar(int id)
         {
+            try
+            {
           bool apagado = await _produtosRepositorio.Apagar(id);
             return Ok(apagado);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
         }
 
     }
--- WebApiEstoque/Controllers/UsuarioControler.cs
+++ WebApiEstoque/Controllers/UsuarioControler.cs
@@ -20,37 +20,78 @@ namespace WebApiEstoque.Controllers
         [HttpGet]
         public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
         {
+            try
+            {
             List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
             return Ok(usuarios);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
         {
+            try
+            {
             UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
             return Ok(usuario);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         [HttpPost]
         public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
         {
+            try
+            {
+
             UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
             return Ok(usuario);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
         {
+            try
+            {
+
             usuarioModel.Id = id;
             UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
             return Ok(usuario);
+
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<UsuarioModel>> Apagar(int id)
         {
+            try
+            {
+
             bool apagado = await _usuarioRepositorio.Apagar(id);
             return Ok(apagado);
+        }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
